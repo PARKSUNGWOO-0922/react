d63684b61e9baa8192e3013cf32336caf765e6c5
PARKSUNGWOO-0922 <mailcrybaby@gmail.com>
2025-12-08 17:58:49 +0900

    자바시작

--- basic/src/ch01_시작하기/ex01_Hello.java
+++ basic/src/ch01_시작하기/ex01_Hello.java
@@ -4,7 +4,7 @@ public class ex01_Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	     System.out.println("Hi,Java");
+	     System.out.println("Hi,Java!");
 
 	}
 
