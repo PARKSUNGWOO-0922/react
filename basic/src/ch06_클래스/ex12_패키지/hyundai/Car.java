e0feeb9a49b542a56723995d7151a3ca98654d1d
PARKSUNGWOO-0922 <mailcrybaby@gmail.com>
2025-12-23 15:05:25 +0900

    게터세터

--- basic/src/ch06_클래스/ex12_패키지/hyundai/Car.java
+++ basic/src/ch06_클래스/ex12_패키지/hyundai/Car.java
@@ -1,12 +1,13 @@
-//패키지 선언
+// 패키지 선언
+// 항상 소스 파일의 최상단에 위치해야 한다.
 package ch06_클래스.ex12_패키지.hyundai;
-//ctrl+shift+o
+
+// 자동 임포트: ctrl+shift+o
 import ch06_클래스.ex12_패키지.hankook.SnowTire;
-//import ch06_클래스.ex12_패키지.kumho.AllSeasonTire;
-import ch06_클래스.ex12_패키지.kumho.*;
+import ch06_클래스.ex12_패키지.hankook.Tire;
 
 /**
-* 26.패키지
+* 26. 패키지(package)
 * 같은 패키지에 있는 클래스는 import 없이 사용할 수 있으나,
 * 다른 패키지에 있는 클래스는 import 문을 이용해서 어떤 패키지의 클래스를 사용하는지 명시해야 한다.
 * import 문이 작성되는 위치는 패키지 선언과 클래스 선언 사이이다.
@@ -15,7 +16,14 @@ import ch06_클래스.ex12_패키지.kumho.*;
 */
 //import 문으로 다른 패키지 클래스 사용을 명시
 //만약 동일한 패키지에 다수의 클래스를 사용하는 경우 클래스명을 생략하고 *을 사용할 수 있다.
-// * 는 패키지안에 모든 클래스를 가르킨다
+// *는 패키지 안의 모든 클래스를 가리킨다.
+import ch06_클래스.ex12_패키지.kumho.AllSeasonTire;
+// The import ch06_클래스.ex12_패키지.kumho.Tire collides(충돌하다) with another import statement
+// import ch06_클래스.ex12_패키지.kumho.Tire; // 에러
+// The import ch06_클래스.ex12_패키지.kumho is never used // 경고
+import ch06_클래스.ex12_패키지.kumho.*;
+
+
 /**
 * 패키지(Package)
 *  - 클래스의 일부분이며, 클래스를 식별하는 용도로 사용된다.
@@ -29,29 +37,38 @@ import ch06_클래스.ex12_패키지.kumho.*;
 *   - 개발 회사 도메인 이름의 역순으로 작성하고, 마지막에 프로젝트명을 붙인다.
 *
 * 		package 상위패키지.하위패키지;
-*
+* 
+* 		예) kr.co.subway.프로젝트명
+*		예) com.naver.프로젝트명
 * 		예) com.samsung.프로젝트명
-* 		예) org.apache.프로젝트명
+* 
 */
 public class Car {
-	// 어떤 패키지의 클래스를 사용할 지 결정할 수 없기 때문에 컴파일 에러 발생!
+	
 	public static void main(String[] args) {
-		
-		Tire tire = new Tire(); // 컴파일 에러
+		Tire tire = new Tire();
 		System.out.println(tire.company);
 		
-		//크래스 전체 이름을 사용할 경우  import 문은 필요없다
-		//클래스 전체이름? 패키지.클래스
-		ch06_클래스.ex12_패키지.hankook.Tire tire1 = new ch06_클래스.ex12_패키지.hankook.Tire();
 		// 클래스 전체 이름을 사용할 경우 import 문은 필요 없다.
+		// 클래스 전체 이름? 패키지.클래스
+		ch06_클래스.ex12_패키지.hankook.Tire tire1 = new ch06_클래스.ex12_패키지.hankook.Tire();
 		ch06_클래스.ex12_패키지.kumho.Tire tire2 = new ch06_클래스.ex12_패키지.kumho.Tire();
-		
 		System.out.println(tire1.company);
 		System.out.println(tire2.company);
-		//클래스명만 사용할 경우 관련 패키지 import한 후 사용한다.
+		
+		// 클래스명만 사용할 경우 관련 패키지를 import 한 후 사용한다. (일반적)
 		SnowTire tire3 = new SnowTire();
 		AllSeasonTire tire4 = new AllSeasonTire();
+		
 		System.out.println(tire3.msg);
-		System.out.println(tire4.msg);	}
+		System.out.println(tire4.msg);
+	}
+	
 }
+
+
+
+
+
+
 
---
+++ basic/src/ch06_클래스/ex13_접근제한자/package1/B.java
@@ -0,0 +1,59 @@
+package ch06_클래스.ex13_접근제한자.package1;
+
+public class B {
+	//default 필드 선언
+	B b1 = new B(false);
+	B b2 = new B(10);
+	B b3 = new B("문자열B");
+	
+	
+	//public 필드
+	public int field1;
+	//default 필드
+	int field2;
+	//private 필드
+	private int field3;
+	
+	
+	//public 생성자
+	public B(boolean b) {
+		
+	}
+	//default 생성자
+	B(int i){
+		
+	}
+	//private 생성자
+	private B(String s) {
+		field1 = 1;
+		field2 = 2;
+		field3 = 3;
+		
+		method1();
+		method2();
+		method3();
+		
+	}
+	
+	//public 메서드
+	public void method1() {}
+	//default 메서드
+	void method2() {}
+	//private 메서드
+	private void method3() {}
+	
+	
+	//A클래스접근 가능
+	//왜? A클래스는 default로 선언
+	//-> 같은 패키지 내에서는 접근 가능
+	//-> A 와 B클래스는 package1로 같은 패키지에 있다.
+	A a;
+	A a1;
+	A a2;
+	//D cannot be resolved to a type
+	//D는 어떤 유형으로도 해석될수 없다
+	//D d1;
+	//D d2;
+	//D d;에러
+	//왜? D클래스는 dafault 선언했고,다른패키지에 있다.
+}
